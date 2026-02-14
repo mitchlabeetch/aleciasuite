38891e40793bc7836c5ba4e4f8a57caab281ce55
mitchlabeetch <editionsrevel@gmail.com>
2026-02-14 06:56:12 +0100

    server added

---
+++ services/cms/config/server.js
@@ -0,0 +1,8 @@
+// services/cms/config/server.js
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+});
